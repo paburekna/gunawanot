00bfc2539ef434d8f6ba0f9b92200b6c9b389945
gunawanot21 <62414918+gunawanot21@users.noreply.github.com>
2020-03-21 00:37:19 +0000

    Add files via upload

---
+++ quiz game with phyton.py
@@ -0,0 +1,58 @@
+import random 
+
+def get_tof_statement() :
+
+    statements = []
+    statements.append(["tigers have a stripes", "T"])
+    statements.append(["the capital of france is madrid", "F"])
+    statements.append(["the queen own one sixth of the land on the earth", "T"])
+
+    return statements
+
+def play_tof_quiz() :
+
+    # get true or false statement
+    get_tof_statements = get_tof_statements()
+
+    # randomise tof statement 
+    random.shuffle (get_tof_statements)
+
+    # set player score to 0
+    score = 0
+
+    # show tof statements using a loop
+    for s in tof_statements:
+        # present statement 
+        print("true or false:" + s [0])
+        # user enter guess
+        guess = input ("Enter T or F: ")
+        # check if guess is correct
+        if guess == s [1]:
+            print ("correct!")
+            # update  score 
+            score = score + 1
+        else:
+            print("incorrect:(")
+    # show final score
+    print("your final score is: " + str(score))
+
+    def main_menu():
+
+        print ("+------------------------------")
+        print ("welcome to quiz master  3000 ! ")
+        print ("+------------------------------")
+        print ("please select an option :      ")
+        print ("                               ")
+        print ("1. play true or false quiz     ")
+        print ("2. play general knowledge quiz ")
+        print ("0. quit                        ")
+        print ("+------------------------------")
+        choice = input ("enter 1, 2 or 0 : ")
+        if choice =="1":
+            play_tof_quiz ()
+        elif choice ==0 :
+            print("thanks for playing !")
+            quit()
+
+
+main_menu ()
